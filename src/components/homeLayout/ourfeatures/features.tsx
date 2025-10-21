1f124b2981e4e4dfe6c3047881579f59db3c933a
HasanurBanna <hasanurbanna03@gmail.com>
2025-10-21 12:49:20 +0600

    features complet

--- src/components/homeLayout/ourfeatures/features.tsx
+++ src/components/homeLayout/ourfeatures/features.tsx
@@ -6,90 +6,113 @@ import HumbertImage from "@/app/assets/featuresimage/humbertholland.png";
 import PatriciaImage from "@/app/assets/featuresimage/patriciamendoza.png";
 import Link from "next/link";
 import { TbAntennaBars5 } from "react-icons/tb";
-import { MdAddIcCall } from "react-icons/md";
+import { MdAddIcCall, MdGroups } from "react-icons/md";
+import { HiSquares2X2 } from "react-icons/hi2";
+import { IoCopy } from "react-icons/io5";
 
 const Features = () => {
   return (
     <>
-      <div className="flex flex-col justify-center px-15 py-15">
-        <div className=" py-10">
-          <h1>
-            <span>Our</span> <span>Features</span>
-          </h1>
-          <p>
-            This very extraordinary feature, can make learning activities more
-            efficient
-          </p>
-        </div>
+      <div className="px-15 py-15">
+        <div className="flex flex-col justify-center">
+          <div className="mb-10 flex flex-col justify-center items-center">
+            <h1 className="text-2xl font-semibold">
+              <span className="text-[#2F327D]">Our</span>{" "}
+              <span className="text-[#00CBB8]">Features</span>
+            </h1>
+            <p className="text-xl text-gray-400">
+              This very extraordinary feature, can make learning activities more
+              efficient
+            </p>
+          </div>
 
-        <section className="grid grid-cols-1 md:grid-cols-2">
-          <div className="bg-blue-400">
-            <div className="bg-red-300 grid grid-flow-col grid-rows-3 gap-2 px-4 py-4">
-              <div className="relative self-center flex items-center justify-center w-[250px] h-[250px] row-span-3 ">
-                <Image
-                  src={EvenyImage}
-                  alt="Image..."
-                  width={700}
-                  height={700}
-                  className="rounded-2xl object-cover"
-                />
-                <div className="absolute left-0 bottom-3 right-0 flex justify-center items-center gap-2 text-white font-semibold">
-                  <TbAntennaBars5 className="text-xl" />
-                  <Link href={"/#"} className="px-1 bg-blue-500 rounded-md">
-                    Instructor
-                  </Link>
-                  <p>Eveny Howard</p>
+          <section className="grid grid-cols-3 gap-6 mx-auto">
+            <div className="col-span-3 lg:col-span-2 flex justify-center items-center">
+              {/* Meating section */}
+              <div className="relative bg-white before:content-[''] before:absolute before:top-0 before:left-0 before:right-0 before:w-full before:h-[35px] before:bg-gray-200 before:rounded-tl-2xl before:rounded-tr-2xl w-[780px] h-[480px] mx-auto grid grid-cols-3 gap-2 pl-5 py-10 justify-center items-center rounded-2xl shadow-[0_0_15px_rgba(0,0,0,0.15)]">
+                {/* part1 image */}
+                <div className="w-[255px] h-[246px] rounded-2xl overflow-hidden">
+                  <Image
+                    src={EvenyImage}
+                    alt="Image..."
+                    className="w-full h-full object-cover"
+                  />
                 </div>
-                <div className="absolute left-0 -bottom-16 right-0 flex justify-center items-center gap-4 py-4 text-white">
-                  <Link
-                    href={"/#"}
-                    className="px-6 py-2 bg-blue-500 rounded-full"
-                  >
-                    Present
-                  </Link>
-                  <Link
-                    href={"/#"}
-                    className="flex justify-center items-center px-6 py-2 gap-2 bg-red-500 rounded-full"
-                  >
-                    <span>
-                      <MdAddIcCall />
-                    </span>
-                    Call
-                  </Link>
+                {/* part2 image */}
+                <div className="flex flex-col justify-center items-center gap-10">
+                  <div className="w-[166px] h-[160px] overflow-hidden rounded-2xl">
+                    <Image
+                      src={TamraraImage}
+                      alt="Image..."
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="w-[166px] h-[160px] overflow-hidden rounded-2xl">
+                    <Image
+                      src={HumbertImage}
+                      alt="Image..."
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className=" relative row-span-3 w-[166px] flex flex-col items-center justify-between">
-                <Image
-                  src={TamraraImage}
-                  alt="Image..."
-                  className="object-cover rounded-2xl absolute left-0 -top-10 right-0"
-                />
-                <Image
-                  src={HumbertImage}
-                  alt="Image..."
-                  className="object-cover rounded-2xl absolute left-0 -bottom-10 right-0"
-                />
-              </div>
-              <div className="relative w-[200px] bg-amber-500 row-span-3 gap-y-4">
-                <Image
-                  src={AdamImage}
-                  alt="Image..."
-                  width={200}
-                  height={200}
-                  className="rounded-2xl object-cover "
-                />
-
-                <Image
-                  src={PatriciaImage}
-                  alt="Image..."
-                  width={200}
-                  height={200}
-                  className="rounded-2xl object-cover "
-                />
+                {/* part3 image */}
+                <div className="flex flex-col justify-center items-center gap-10">
+                  <div className="w-[202px] h-[195px] overflow-hidden rounded-2xl">
+                    <Image
+                      src={AdamImage}
+                      alt="Image..."
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="w-[202px] h-[195px] overflow-hidden rounded-2xl">
+                    <Image
+                      src={PatriciaImage}
+                      alt="Image..."
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+            {/* Information section */}
+            <div className="flex flex-col justify-center items-center space-y-10 col-span-3 lg:col-span-1">
+              {/* interface */}
+              <div>
+                <h1 className="text-[#2F327D] text-4xl">
+                  A <span className="text-[#00CBB8]">user interface</span>{" "}
+                  designed for the classroom
+                </h1>
+              </div>
+              {/* icon and information */}
+              <div className="flex flex-col justify-center items-center gap-16">
+                <div className="flex justify-center items-center gap-4 text-2xl">
+                  <div>
+                    <HiSquares2X2 className="bg-white shadow-[0_0_15px_rgba(0,0,0,0.15)] w-10 h-10 p-2 rounded-full" />
+                  </div>
+                  <div>
+                    <p className="text-gray-400">{`Teachers don't get lost in the grid view and have a dedicated podium space.`}</p>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center gap-4 text-2xl">
+                  <div>
+                    <IoCopy className="bg-white shadow-[0_0_15px_rgba(0,0,0,0.15)] w-10 h-10 p-2 rounded-full" />
+                  </div>
+                  <div>
+                    <p className="text-gray-400">{`TA's and presenters can be moved to the front of the class.`}</p>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center gap-4 text-2xl">
+                  <div>
+                    <MdGroups className="bg-white shadow-[0_0_15px_rgba(0,0,0,0.15)] w-10 h-10 p-2 rounded-full" />
+                  </div>
+                  <div>
+                    <p className="text-gray-400">{`Teachers can easily see all students and class data at one time.`}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     </>
   );
